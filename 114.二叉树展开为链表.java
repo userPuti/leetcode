8123acc2148fda76e357acf074daa98b97a82be9
Puti <349251265@qq.com>
2022-03-18 20:16:44 +0800

    114

---
+++ 114.二叉树展开为链表.java
@@ -0,0 +1,53 @@
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=114 lang=java
+ *
+ * [114] 二叉树展开为链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+
+        fun1(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode pre = queue.poll();
+            TreeNode curr = queue.peek();
+            pre.left = null;
+            pre.right = curr;
+        }
+    }
+
+    private void fun1(TreeNode root) {
+        if (root == null)
+            return;
+        queue.add(root);
+
+        fun1(root.left);
+        fun1(root.right);
+    }
+}
+// @lc code=end
