4e878d02459acbd517a04014af060e5c60f5759e
Puti <349251265@qq.com>
2022-03-18 19:29:57 +0800

    116

---
+++ 116.填充每个节点的下一个右侧节点指针.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=116 lang=java
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null)
+            return null;
+
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Node temp = queue.poll();
+                if (i < size - 1) {
+                    temp.next = queue.peek();
+                }
+
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+        }
+
+        return root;
+
+    }
+}
+// @lc code=end
